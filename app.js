8aa7b32a50c45845a208829bcb1b6f0139689f58
gerwyn <gerwynsim@hotmail.com>
2025-08-30 20:46:33 +0800

    create task testing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const connectDB = require("./db/connect");
-
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config()
 
 // middleware
 app.use(express.json());
@@ -13,7 +13,7 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://gerwyn:1234@next-store.m1pf79i.mongodb.net/";
-
 const connectDB = async (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
